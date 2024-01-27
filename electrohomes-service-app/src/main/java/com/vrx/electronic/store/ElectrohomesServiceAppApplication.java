5cd79520c9afe6b32f65c0ade37e06d06d89986d
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2024-01-27 22:14:20 +0530

    initial files for backend service added

---
+++ electrohomes-service-app/src/main/java/com/vrx/electronic/store/ElectrohomesServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.vrx.electronic.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrohomesServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrohomesServiceAppApplication.class, args);
+	}
+
+}
